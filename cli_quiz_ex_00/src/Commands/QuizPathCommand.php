ff28761fca88ea80eca29bcc1d19175923326eb9
Abdul-Harisu Inusah <inusahharis@gmail.com>
2019-06-25 23:06:38 +0000

    refactor get question

--- cli_quiz_ex_00/src/Commands/QuizPathCommand.php
+++ cli_quiz_ex_00/src/Commands/QuizPathCommand.php
@@ -21,21 +21,17 @@ class QuizPathCommand extends Command
         $this
         ->setName("start")
         ->setDescription("Start quiz")
-        ->addArgument("quiz_name")
         ->addOption("filepath");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $correctAnswers = 0;
-        // $path = $input->getArgument("quiz_name");
         $path = $input->getOption("filepath");
-        $path  = empty($path) ? $this->defaultFilePath : $path;
-        $csvQuestions = array_map('str_getcsv', file('problems.csv'));
+        $csvQuestions = $this->getQuestions($path);
         $numberOfQuestions = count($csvQuestions);
 
         $output->writeln("<info>Answer the following questions?</info>");
-        $output->writeln("");
         $helper = $this->getHelper("question");
         foreach($csvQuestions as $question){
             $answer = $question[1];
@@ -49,6 +45,20 @@ class QuizPathCommand extends Command
         }
         $output->writeln("");
         $output->writeln("<info>You got $correctAnswers out of $numberOfQuestions</info>");
+    }
+
+    /*
+    * Parses the csv file for the problems given the filepath 
+    *
+    * @param string filePath    Takes location of questions
+    *
+    * @return {mixed} array
+    */
+    protected function getQuestions(string $filePath): array
+    {
+        $path  = empty($filePath) ? $this->defaultFilePath : $filePath;
+        $csvQuestions = array_map('str_getcsv', file($path));
+        return $csvQuestions;
     }
 }
 ?>
